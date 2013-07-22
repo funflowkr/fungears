95a8271641e49eabb02ec686841ff16b6d425653
ipknHama <ipknhama@gmail.com>
2013-07-22 19:13:43 +0000

    테스트 서버 정보도 수집하게, 빈 데이터는 저장하지않게

--- collect/collect.py
+++ collect/collect.py
@@ -17,12 +17,20 @@ cursor = dbconn.cursor()
 #cursor.execute('drop table ranking_stat')
 #cursor.execute('create table ranking_stat (id int primary key auto_increment, whenadded datetime, gamename varchar(250) not null, body text)')
 params = []
+for item in conn.select('game_test', 'select * from game_test'):
+	api_usages = {}
+	for key, value in item.iteritems():
+		if key[:2] == 'a_':
+			api_usages[key[2:]] = value
+	if api_usages:
+		params.append(('_'+str(item.name), json.dumps(api_usages)))
 for item in conn.select('game', 'select * from game'):
 #for item in conn.select('game_test', 'select * from game_test'):
 	api_usages = {}
 	for key, value in item.iteritems():
 		if key[:2] == 'a_':
 			api_usages[key[2:]] = value
-	params.append((str(item.name), json.dumps(api_usages)))
+	if api_usages:
+		params.append((str(item.name), json.dumps(api_usages)))
 cursor.executemany('insert into ranking_stat(whenadded, gamename, body) values(now(), %s, %s)', params)
 dbconn.commit()
